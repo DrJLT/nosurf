bd10b6ad360d24aae3f13974981c9326f1d62b0b
DrJLT <DrJLT@jlteng.com>
2020-10-12 19:05:26 +0200

    small further optimization

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # nosurf
 
 This is a stripped-down version of the nosurf package by justinas. I do not recommend it for use without modification.
+
+All credits go to the original authors.
--- token.go
+++ token.go
@@ -13,30 +13,20 @@ const (
 
 func generateToken() []byte {
 	bytes := make([]byte, tokenLength)
-
 	if _, err := io.ReadFull(rand.Reader, bytes); err != nil {
 		panic(err)
 	}
-
 	return bytes
 }
 
-// verifyToken expects the realToken to be unmasked and the sentToken to be masked
 func verifyToken(realToken, sentToken []byte) bool {
 	realN := len(realToken)
 	sentN := len(sentToken)
-
+	unmasked := unmaskToken(sentToken)
 	if realN == tokenLength && sentN == 2*tokenLength {
-		return tokensEqual(realToken, unmaskToken(sentToken))
+		return len(unmasked) == tokenLength && subtle.ConstantTimeCompare(realToken, unmasked) == 1
 	}
 	return false
-}
-
-// tokensEqual expects both tokens to be unmasked
-func tokensEqual(realToken, sentToken []byte) bool {
-	return len(realToken) == tokenLength &&
-		len(sentToken) == tokenLength &&
-		subtle.ConstantTimeCompare(realToken, sentToken) == 1
 }
 
 func init() {
@@ -47,22 +37,3 @@ func init() {
 		panic(fmt.Sprintf("crypto/rand is unavailable: Read() failed with %#v", err))
 	}
 }
-
-// VerifyToken verifies the sent token equals the real one
-// func VerifyToken(realToken, sentToken string) bool {
-// 	r, err := base64.StdEncoding.DecodeString(realToken)
-// 	if err != nil {
-// 		return false
-// 	}
-// 	if len(r) == 2*tokenLength {
-// 		r = unmaskToken(r)
-// 	}
-// 	s, err := base64.StdEncoding.DecodeString(sentToken)
-// 	if err != nil {
-// 		return false
-// 	}
-// 	if len(s) == 2*tokenLength {
-// 		s = unmaskToken(s)
-// 	}
-// 	return tokensEqual(r, s)
-// }
